eac34d7922e957b1621dcf192ef161ae7286fafb
shenzhenyuan <szywind@163.com>
2016-11-01 23:56:57 -0500

    finish part 2&3 and pass test 3,4,5

--- Projects/switchyard-master/examples/exercises/router/myrouter.py
+++ Projects/switchyard-master/examples/exercises/router/myrouter.py
@@ -64,16 +64,22 @@ from switchyard.lib.common import *
 class WaitingARP(object):
     def __init__(self, times = 4):
         self.nLeftRetries = times
-        self.time = time.time()
+        self.setTime()
 
-    def isTimeout(self, time=time.time()):
-        return self.time + 1 <= time
+    def setTime(self):
+        self.ltime = time.time()
 
-    def isValid(self, time=time.time()):
-        return self.nLeftRetries > 0 or (self.nLeftRetries == 0 and not self.isTimeout(time))
+    def getTime(self):
+        return self.ltime
+
+    def isTimeout(self, curtime):
+        return self.getTime() + 1 <= curtime
+
+    def isValid(self, curtime):
+        return self.nLeftRetries > 0 or (self.nLeftRetries == 0 and not self.isTimeout(curtime))
 
     def update(self):
-        self.time = time.time()
+        self.setTime()
         self.nLeftRetries -= 1
 
     def setInvalid(self):
@@ -94,40 +100,41 @@ class Router(object):
     def setupFT(self):
         ## build FT from local file
         try:
-            with open("./forwarding_table") as fl:
+            dir_path = os.path.dirname(os.path.realpath(__file__))
+            with open(dir_path + "/forwarding_table.txt", "r") as fl:
                 # for line in fl:
                 #     item = line.strip().split(' ')
                 #     self.forwardTable[int(item[0]) & int(item[1])] = item[1:]
                 # self.tableForward = {[0]: (lambda x: x.strip().split(' '))(line)[1:] for line in fl}
 
                 # self.tableForward.extend(parseLine(line) for line in fl)
-                self.tableForward.extend((lambda x: x.strip().split(' '))(line) for line in fl)
-
+                self.forwardTable.extend([(lambda x: x.strip().split(' '))(line) for line in fl])
         except FileNotFoundError:
             pass
 
-        ## build FT from router configurations
-        for intf in self.interfaces:
-            ipaddr = IPv4Address(intf.ipaddr)
-            netmask = IPv4Address(intf.netmask)
+        finally:
+            ## build FT from router configurations
+            for intf in self.interfaces:
+                ipaddr = IPv4Address(intf.ipaddr)
+                netmask = IPv4Address(intf.netmask)
 
-            prefix = IPv4Address(int(ipaddr) & int(netmask))
-            self.forwardTable.append([str(prefix), str(netmask), None, intf.name])
+                prefix = IPv4Address(int(ipaddr) & int(netmask))
+                self.forwardTable.append([str(prefix), str(netmask), None, intf.name])
 
-            # assert int(ipaddr) == int(ipaddr) & int(netmask)
-            # self.forwardTable.append([str(ipaddr), str(netmask), None, intf.name])
+                # assert int(ipaddr) == int(ipaddr) & int(netmask)
+                # self.forwardTable.append([str(ipaddr), str(netmask), None, intf.name])
 
     def lookupFT(self, ipaddr):
         destaddr = IPv4Address(ipaddr)
         ans = None
         maxLength = -1
         for item in self.forwardTable:
-            prefix = IPv4Address(item[0])
-            matches = (int(prefix) & int(destaddr)) == int(prefix)
+            prefixnet = IPv4Network(item[0]+'/'+item[1])
 
-            netaddr = IPv4Network(item[0]+'/'+item[1])
-            if matches and netaddr.prefixlen > maxLength:
+            matches = destaddr in prefixnet
+            if matches and prefixnet.prefixlen > maxLength:
                 ans = item
+                maxLength = prefixnet.prefixlen
         return ans
 
     def forwarding(self, pkt):
@@ -154,7 +161,7 @@ class Router(object):
                     nextHopIP = ippkt.dstip
                 else:
                     ## next hop is another router
-                    nextHopIP = fwd_info[2]   # assert(dstIP is not None)
+                    nextHopIP = IPv4Address(fwd_info[2])   # assert(dstIP is not None)
 
                 nextHopThruPortName = fwd_info[3]
                 nextHopThruPort = self.net.interface_by_name(nextHopThruPortName)
@@ -163,18 +170,9 @@ class Router(object):
                     ## lookup the dst. MAC
                     nextHopMAC =  self.ipMacTable[nextHopIP]
 
-                    ## unencapsulate Ethernet header
-                    # ether_index = pkt.get_header_index(Ethernet)
-                    # ether_type = pkt[ether_index].ethertype
-                    # del pkt[ether_index]
-
-                    # ether = Ethernet()
                     ether = pkt.get_header(Ethernet)
                     ether.src = EthAddr(nextHopThruPort.ethaddr)
                     ether.dst = EthAddr(nextHopMAC)
-                    # ether.ethertype = ether_type
-
-                    # fwdpkt = ether + pkt
                     fwdpkt = pkt
                     self.net.send_packet(nextHopThruPortName, fwdpkt)
                 else:
@@ -196,13 +194,13 @@ class Router(object):
         while True:
             gotpkt = True
             try:
-                input_port,pkt = self.net.recv_packet(timeout=1.0)
+                input_port,pkt = self.net.recv_packet()
             except NoPackets:
                 log_debug("No packets available in recv_packet")
                 gotpkt = False
             except Shutdown:
                 log_debug("Got shutdown signal")
-                break
+                return
 
             if gotpkt:
                 log_debug("Got a packet: {}".format(str(pkt)))
@@ -236,33 +234,28 @@ class Router(object):
                         self.ipMacTable[nextHopIP] = nextHopMAC
 
                         for (arp_i, nextHopThruPort, nextHopIP2, oldfwdpkt) in self.queue:
-                            if nextHopIP2 == nextHopIP:
-                                if arp_i.isValid() and not arp_i.isTimeout():
-                                    ## unencapsulate Ethernet header
-                                    # ether_index = oldfwdpkt.get_header_index(Ethernet)
-                                    # ether_type = oldfwdpkt[ether_index].ethertype
-                                    # del oldfwdpkt[ether_index]
+                            if str(nextHopIP2) == str(nextHopIP):
+                                curTime = time.time()
+                                if arp_i.isValid(curTime) and not arp_i.isTimeout(curTime):
 
                                     ether = oldfwdpkt.get_header(Ethernet)
-                                    # ether = Ethernet()
                                     ether.src = EthAddr(nextHopThruPort.ethaddr)
                                     ether.dst = EthAddr(nextHopMAC)
-                                    # ether.ethertype = ether_type
-
-                                    # fwdpkt = ether + oldfwdpkt
                                     fwdpkt = oldfwdpkt
                                     self.net.send_packet(nextHopThruPort.name, fwdpkt)
                                     arp_i.setInvalid()
 
                 ## Receive IP packet
                 else:
+                    assert pkt.get_header(Ethernet).ethertype == EtherType.IP
                     self.forwarding(pkt)
-
+                # debugger()
             ## maintain the waiting ARP queue
             for (arp_i, nextHopThruPort, nextHopIP, _) in self.queue:
-                if not arp_i.isValid():
+                curTime = time.time()
+                if not arp_i.isValid(curTime):
                     continue
-                if not arp_i.isTimeout():
+                if not arp_i.isTimeout(curTime):
                     continue
                 else:
                     ## send ARP request
@@ -273,7 +266,8 @@ class Router(object):
                     arp_i.update()
 
             ## drop ARP pkt after retrying 5 times w/o receiving ARP response
-            self.queue = [item_j for item_j in self.queue if item_j[0].isValid()]
+            curTime = time.time()
+            self.queue = [item_j for item_j in self.queue if item_j[0].isValid(curTime)]
 
 def switchy_main(net):
     '''
